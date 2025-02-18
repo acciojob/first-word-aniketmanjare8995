e5f1c2686b1161b9a4cc037c2d8ba37cd3e92115
aniketmanjare8995 <aniketmanjare8712@gmail.com>
2025-02-18 15:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstWord(s) {
   // your code here
+  s= s.trim();
+
+    // Find the first space in the string
+    let spaceIndex = s.indexOf(" ");
+
+    // If there's no space, return the entire string
+    if (spaceIndex === -1) {
+        return s;
+    }
+
+    // Otherwise, return the substring up to the first space
+    return s.substring(0, spaceIndex);
+
+
 }
 
 // Do not change the code below
